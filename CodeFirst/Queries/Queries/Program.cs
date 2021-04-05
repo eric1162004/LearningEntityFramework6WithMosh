dbb31b9c1d87e5b82cab63bb64518fd2792ee4ec
Eric Cheung <000437017@student.vcc.ca>
2021-04-04 23:10:01 -0700

    added course notes

---
+++ CodeFirst/Queries/Queries/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+
+namespace Queries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new PlutoContext();
+
+            var course = new Course
+            {
+                Name = "New Course",
+                Description = "New Description",
+                FullPrice = 19.95f,
+                Level = 1,
+                Author = new Author { Id = 1, Name = "Mosh" }
+            };
+
+            context.Courses.Add(course);
+
+            context.SaveChanges();
+
+
+
+
+
+
+
+
+            System.Console.ReadLine();
+
+        }
+    }
+}
